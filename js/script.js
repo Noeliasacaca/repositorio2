bdf432d495999480879d1a00d39181b06709120b
Noeliasacaca <107666926+Noeliasacaca@users.noreply.github.com>
2022-08-03 21:00:59 -0300

    agregando primera entrega

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-/* let usuario = "maria36";
+let usuario = "maria36";
+
 
 function login() {
     let ingresar = false;
-
     for (let i = 2; i >= 0; i--) {
         let sesion = prompt("introdusca su usuario, para iniciar sesión ");
         if (sesion === usuario) {
@@ -14,10 +14,10 @@ function login() {
         }
     }
 
-let contraseña = "amarillo";
+    let contraseña = "amarillo";
 
     for (let i = 2; i >= 0; i--) {
-        let ingresar = prompt( "Ingresá tu contraseña ");
+        let ingresar = prompt("Ingresá tu contraseña ");
         if (ingresar === contraseña) {
             alert("sesión iniciada con exito")
             alert("Gracias por elegir nuestra tienda <3")
@@ -25,8 +25,8 @@ let contraseña = "amarillo";
             ingresar = true;
             break;
         } else {
-            alert("Contraseña incorrecta, intentelo de nuevo. Te quedan "  + i +  "intentos.");
-            
+            alert("Contraseña incorrecta, intentelo de nuevo. Te quedan " + i + "intentos.");
+
         }
     }
 
@@ -44,74 +44,59 @@ if (login()) {
                 break;
             case '2':
                 alert("tu compra fue Tetera de porcelada con forma de caracol  de $1.500 .")
-               break;
+                break;
             case '3':
                 alert("tu compra fue Cenicero diseño de gatito de $1.000.")
                 break;
-
         }
         productos = prompt("Elegí el en producto que desea comprar : \n1- Juego de tazas con figuras de fresas de $2.000 \n2 - Tetera de porcelada con forma de caracol  de $1.500 \n3 - Cenicero diseño de gatito de $1.000 \nPresioná X para finalizar.");
     }
 }
 
-
-alert ("gracias por elegisnos") */
+alert("gracias por elegirnos")
 
 
 
 class ceramica {
 
-    constructor(producto, precio, stock,  id) {
+    constructor(producto, precio, stock) {
         this.producto = producto;
         this.precio = parseInt(precio);
         this.stock = stock;
-        this.id = id;
     }
 
-    asignarId(array) {
-        this.id = array.length;
+    asignarproductos(array) {
+        this.producto = array.length;
     }
 
 }
 
 const ceramicas = [
-    new ceramica("taza con diseño de sandia", 1300, 'con stok', 1),
-    new ceramica("cenicero con diseño de corazón", 1000, 'con stok', 3),
-    new ceramica("tetera con forma de gatito", 2100, 'sin stok', 4),
-    new ceramica("sahumerio con forma de hoja", 1500, 'con stok', 5),
-    new ceramica("maceta con forma de frutilla", 1800, 'con stok', 6)
+    new ceramica("taza con diseño de sandia", 1300, 'con stock'),
+    new ceramica("cenicero con diseño de corazón", 1000, 'con stock'),
+    new ceramica("tetera con forma de gatito", 2100, 'sin stok'),
+    new ceramica("porta sahumerio con forma de hoja", 1500, 'con stock'),
+    new ceramica("maceta con forma de frutilla", 1800, 'con stock'),
+    new ceramica("cenicero franjas Rojas", 1200, 'con stock'),
+    new ceramica("cenicero con forma de huevo", 1700, 'con stock'),
+    new ceramica("porta sahumerio con forma de hongo", 1600, 'con stock'),
+    new ceramica("tetera con forma de caracol", 2300, 'con stock'),
+    new ceramica("taza diseño con cuadros", 900, 'sin stock'),
+    new ceramica("tetera forma de pera", 2400, 'sin stock'),
+    new ceramica("velero Forma De Hongo", 1100, 'con stock'),
+    new ceramica("velero calavera", 1300, 'con stock')
 ]
 
 console.log(ceramicas);
 
-let categoria = prompt('Elegí lo que buscas:\n1 - productos (A a Z) \n2 - productos (Z a A) \n3 - precio');
 
-function ordenar(categoria, array) {
-    let arrayOrdenado = array.slice(0);
+let productoElegido = prompt('Escribí el nombre  del producto que buscas');
 
+const filtrado = ceramicas.filter((ceramica) => ceramica.producto.toLowerCase().includes(productoElegido.toLowerCase()))
 
-    switch (categoria) {
-        case '1':
-            let deMenoraMayor = arrayOrdenado.sort((a,b)=>a.producto.localeCompare(b.producto));
-            return deMenoraMayor;
-        case '2':
-            let deMayoraMenor = arrayOrdenados.sort((a, b) => b.producto.localeCompare(a.producto));
-            return deMayoraMenor;
-        case '3':
-            return arrayOrdenado.sort((a, b) => b.precio - a.precio);
-        default:
-            alert('error');
-            break;
-    }
+if (filtrado.length == 0) {
+    alert('Lo sentimos. No encontramos coincidencias en nuestro catálogo');
+} else {
+    const imprimible = filtrado.map((ceramica) => ceramica.producto);
+    alert('Los productos de nuestro catálogo, que coinciden parcial o totalmente con esta búsqueda, son:\n- ' + imprimible.join('\n- '));
 }
-function buscador(array){
-    let info = '';
-
-    array.forEach(elemento=>{
-        info += 'producto: ' + elemento.producto + '\nprecio: ' + elemento.precio + '\nstock: ' + elemento.stock + ' puntos.\n\n'
-    })
-
-    return info;
-}
-
-alert(buscador(ordenar(categoria,ceramicas))); 
